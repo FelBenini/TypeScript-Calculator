235f560587862b859a128605811280b71235998b
FelBenini <felbenini@gmail.com>
2023-01-16 21:43:04 -0300

    modified:   script.ts 	modified:   .gitignore

--- script.ts
+++ script.ts
@@ -1,10 +1,15 @@
 const display : HTMLElement = document.getElementById("display")!
+const clear_all : HTMLElement = document.getElementById("ac")!
 const append : HTMLElement = document.getElementById("plus")!
 const test : any = document.querySelectorAll(".button-number")!
 const equals : HTMLElement = document.getElementById("equals")!
 
 append.addEventListener("click", function() {
     display.textContent += "+"
+})
+
+clear_all.addEventListener("click", function() {
+    display.textContent = ""
 })
 
 test.forEach((button) => {
