4a2cde2d7faceae7953c179ad6e3cf0e4d456c5a
FelBenini <felbenini@gmail.com>
2023-01-16 21:43:35 -0300

    modified:   .gitignore

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var display = document.getElementById("display");
+var clear_all = document.getElementById("ac");
 var append = document.getElementById("plus");
 var test = document.querySelectorAll(".button-number");
 var equals = document.getElementById("equals");
 append.addEventListener("click", function () {
     display.textContent += "+";
+});
+clear_all.addEventListener("click", function () {
+    display.textContent = "";
 });
 test.forEach(function (button) {
     button.addEventListener("click", function () {
