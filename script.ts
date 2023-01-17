a63c1f7c5cb4e1fe39487bdb0781c5ff2106a817
FelBenini <felbenini@gmail.com>
2023-01-16 22:05:00 -0300

    modified:   script.ts

--- script.ts
+++ script.ts
@@ -1,18 +1,21 @@
 const display : HTMLElement = document.getElementById("display")!
 const clear_all : HTMLElement = document.getElementById("ac")!
 const append : HTMLElement = document.getElementById("plus")!
-const test : any = document.querySelectorAll(".button-number")!
+const numbers : any = document.querySelectorAll(".button-number")!
 const equals : HTMLElement = document.getElementById("equals")!
 
+//Operations
+
 append.addEventListener("click", function() {
-    display.textContent += "+"
-})
+    if (!isOperation() && !isMultiply()) {
+        display.textContent += "+"
+    }})
 
 clear_all.addEventListener("click", function() {
     display.textContent = ""
 })
 
-test.forEach((button) => {
+numbers.forEach((button) => {
     button.addEventListener("click", function() {
         display.textContent += button.textContent
     })
@@ -21,3 +24,19 @@ test.forEach((button) => {
 equals.addEventListener("click", function() {
     display.textContent = eval(display.textContent)
 })
+
+function isOperation() {
+    if (display.textContent?.slice(-1) === "+") {
+        return true
+    } else if (display.textContent?.slice(-1) === "-") {
+        return true
+    } else return false
+}
+
+function isMultiply() {
+    if (display.textContent?.slice(-1) === "/") {
+        return true;
+    } else if (display.textContent?.slice(-1) === "*") {
+        return true
+    } else return false
+}
